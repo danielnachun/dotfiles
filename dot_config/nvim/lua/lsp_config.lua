9a9d473ae4345201f455086c35ae09a695c4997a
danielnachun <daniel.nachun@gmail.com>
2023-02-08 11:18:46 -0800

    lsp_config.lua: add bash language server

--- dot_config/nvim/lua/lsp_config.lua
+++ dot_config/nvim/lua/lsp_config.lua
@@ -2,6 +2,7 @@
 --local capabilities = require('cmp_nvim_lsp').default_capabilities()
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 require'lspconfig'.r_language_server.setup{}
+require'lspconfig'.bashls.setup{}
 
 --require'navigator'.setup({
 --    lsp = {
@@ -14,6 +15,7 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
     sources = {
+        null_ls.builtins.code_actions.shellcheck
         null_ls.builtins.formatting.styler,
         null_ls.builtins.formatting.format_r
     },
