35f6d08b4e8394d1cba2e9bf26ff4c290fc21949
danielnachun <daniel.nachun@gmail.com>
2023-02-08 11:23:49 -0800

    lsp_config.lua: add bash language server

--- dot_config/nvim/lua/lsp_config.lua
+++ dot_config/nvim/lua/lsp_config.lua
@@ -15,7 +15,7 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
     sources = {
-        null_ls.builtins.code_actions.shellcheck
+        null_ls.builtins.code_actions.shellcheck,
         null_ls.builtins.formatting.styler,
         null_ls.builtins.formatting.format_r
     },
