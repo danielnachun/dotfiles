d60825964bb0c27cd26cac8d6e38fec8292c41fb
danielnachun <daniel.nachun@gmail.com>
2024-06-19 12:28:46 -0700

    use rnvim

---
+++ dot_config/nvim/lua/r_nvim_config.lua
@@ -0,0 +1,8 @@
+require("r").setup({
+    nvimpager = "split_v",
+    rconsole_width = 0,
+    Rout_more_colors = true,
+    esc_term = false,
+    hl_term = false,
+    require("r").setup(opts)
+})
