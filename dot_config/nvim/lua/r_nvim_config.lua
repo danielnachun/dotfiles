e8b9666b54b43a942ce8acc2451f619fdd0776a3
danielnachun <daniel.nachun@gmail.com>
2024-06-21 13:45:22 -0700

    fix for nvim 0.10

--- dot_config/nvim/lua/r_nvim_config.lua
+++ dot_config/nvim/lua/r_nvim_config.lua
@@ -4,5 +4,16 @@ require("r").setup({
     Rout_more_colors = true,
     esc_term = false,
     hl_term = false,
-    require("r").setup(opts)
+    hook = {
+        on_filetype = function ()
+            vim.api.nvim_buf_set_keymap(0, "v", "<Space>", "<Plug>RDSendSelection", {})
+            vim.api.nvim_buf_set_keymap(0, "n", "<Space>", "<Plug>RDSendLine", {})
+            vim.api.nvim_buf_set_keymap(0, "n", "<LocalLeader>rk", "<Cmd>lua require('r.run').action('levels')<CR>", {})
+            vim.api.nvim_buf_set_keymap(0, "n", "<LocalLeader>t", "<Cmd>lua require('r.run').action('tail')<CR>", {})
+            vim.api.nvim_buf_set_keymap(0, "n", "<LocalLeader>h", "<Cmd>lua require('r.run').action('head')<CR>", {})
+            vim.api.nvim_buf_set_keymap(0, "n", "<LocalLeader>p", "<Cmd>lua require('r.run').action('print')<CR>", {})
+            vim.api.nvim_buf_set_keymap(0, "n", "<LocalLeader>d", "<Cmd>lua require('r.run').action('dim')<CR>", {})
+            vim.api.nvim_buf_set_keymap(0, "n", "<LocalLeader>s", "<Cmd>lua require('r.run').action('str')<CR>", {})
+        end
+    },
 })
