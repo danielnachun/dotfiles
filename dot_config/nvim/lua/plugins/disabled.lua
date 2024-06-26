146d43e40b8cb391f7a351d3f6419e1abdfae4d2
danielnachun <daniel.nachun@gmail.com>
2024-06-26 11:02:53 -0700

    use lazyvim

---
+++ dot_config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,9 @@
+return {
+  { "williamboman/mason-lspconfig.nvim", enabled = false },
+  { "williamboman/mason.nvim", enabled = false },
+  { "jay-babu/mason-nvim-dap.nvim", enabled = false },
+  { "mfusenegger/nvim-dap-python", enabled = false },
+  { "catppuccin/nvim", enabled = false },
+  { "catppuccin/nvim", enabled = false },
+  { "folke/tokyonight.nvim", enable = false },
+}
