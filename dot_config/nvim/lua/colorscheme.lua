3dbb95c19c13afffad5d6ea1e6641e7d8499e046
danielnachun <daniel.nachun@gmail.com>
2024-06-26 11:02:31 -0700

    use lazyvim

--- dot_config/nvim/lua/colorscheme.lua
+++
@@ -1,5 +0,0 @@
-require('base16-colorscheme').with_config { telescope = false }
-vim.cmd("colorscheme base16-one-light")
--- Override rOperator link for base16
--- vim.cmd("hi def link rOperator Statement")
-
