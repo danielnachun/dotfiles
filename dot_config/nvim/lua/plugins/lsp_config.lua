cd9ac0e684e502a1c200bb8b07359220ce04ce13
danielnachun <daniel.nachun@gmail.com>
2024-07-03 16:14:58 -0700

    add remaining configs

---
+++ dot_config/nvim/lua/plugins/lsp_config.lua
@@ -0,0 +1,47 @@
+return {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+        opts.servers = vim.tbl_extend(opts.servers, {
+            autotools_ls = {},
+            awk_ls = {},
+            azure_pipelines_ls = {},
+            clojure_lsp = {},
+            --cmake = {},
+            digestif = {},
+            dockerls = {},
+            dprint = {},
+            erg = {},
+            esbonio = {},
+            fortls = {},
+            fsautocomplete = {},
+            golangci_lint_ls = {},
+            harper_ls = {},
+            --jedi_language_server = {},
+            jqls = {},
+            jsonnet_ls = {},
+            lemminx = {},
+            ltex = {}, --compare to texlab
+            luau = {},
+            marksman = {},
+            mesonlsp = {},
+            ocamllsp = {},
+            --perlls = {}, compare to perlpls
+            perlpls = {},
+            prosemd = {},
+            pylyzer = {},
+            remark_ls = {},
+            --rubocop = {}, probably handled by solargraph
+            --rust_analyzer = {}, handeld by rustacean?
+            serve_d = {},
+            spectral = {},
+            sqlls = {},
+            --standardrb = {}, handled by solargraph?
+            steep = {},
+            syntax_tree = {},
+            ttags = {},
+            typeprof = {},
+            vale_ls = {},
+        })
+    end
+
+}
---
+++ dot_config/nvim/lua/plugins/r_nvim_config.lua
@@ -0,0 +1,23 @@
+return {
+    config = function()
+        require("r").setup({
+            nvimpager = "split_v",
+            rconsole_width = 0,
+            Rout_more_colors = true,
+            esc_term = false,
+            hl_term = false,
+            hook = {
+                on_filetype = function ()
+                    vim.api.nvim_buf_set_keymap(0, "v", "<Space>", "<Plug>RDSendSelection", {})
+                    vim.api.nvim_buf_set_keymap(0, "n", "<Space>", "<Plug>RDSendLine", {})
+                    vim.api.nvim_buf_set_keymap(0, "n", "<LocalLeader>rk", "<Cmd>lua require('r.run').action('levels')<CR>", {})
+                    vim.api.nvim_buf_set_keymap(0, "n", "<LocalLeader>t", "<Cmd>lua require('r.run').action('tail')<CR>", {})
+                    vim.api.nvim_buf_set_keymap(0, "n", "<LocalLeader>h", "<Cmd>lua require('r.run').action('head')<CR>", {})
+                    vim.api.nvim_buf_set_keymap(0, "n", "<LocalLeader>p", "<Cmd>lua require('r.run').action('print')<CR>", {})
+                    vim.api.nvim_buf_set_keymap(0, "n", "<LocalLeader>d", "<Cmd>lua require('r.run').action('dim')<CR>", {})
+                    vim.api.nvim_buf_set_keymap(0, "n", "<LocalLeader>s", "<Cmd>lua require('r.run').action('str')<CR>", {})
+                end
+            },
+        })
+    end
+}
