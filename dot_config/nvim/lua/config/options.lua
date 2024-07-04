c53d454cb27cd045d6d35d33d2adcee584906ef5
danielnachun <daniel.nachun@gmail.com>
2024-07-03 19:17:56 -0700

    update configs

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -2,6 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 local opt = vim.opt
+-- opt.signcolumn = "no"
 opt.guicursor = ""
 opt.tabstop = 4
 opt.shiftwidth = 4
@@ -20,48 +21,48 @@ opt.relativenumber = false
 opt.mousemoveevent = true
 
 function set_var(args)
-  vim.api.nvim_set_var(args.variable, args.value)
+	vim.api.nvim_set_var(args.variable, args.value)
 end
 
 set_var({
-  variable = "mapleader",
-  value = ",",
+	variable = "mapleader",
+	value = ",",
 })
 
 set_var({
-  variable = "maplocalleader",
-  value = ",",
+	variable = "maplocalleader",
+	value = ",",
 })
 
 -- Vimtex
 -- Use NeoVim remote to allow feedback about errors
 set_var({
-    variable = "vimtex_compiler_progname",
-    value = "nvr"
+	variable = "vimtex_compiler_progname",
+	value = "nvr",
 })
 
--- Auto fold 
+-- Auto fold
 set_var({
-    variable = "vimtex_fold_enabled",
-    value = 1
-})
-
-set_var({
-    variable = "vimtex_view_method",
-    value = "skim"
+	variable = "vimtex_fold_enabled",
+	value = 1,
 })
 
 set_var({
-    variable = "vimtex_syntax_conceal_disable",
-    value = 1
+	variable = "vimtex_view_method",
+	value = "skim",
+})
+
+set_var({
+	variable = "vimtex_syntax_conceal_disable",
+	value = 1,
 })
 
 -- fix me!
 --let g:vimtex_compiler_latexmk = {
-    --\ 'backend' : 'nvim',
-    --\}
+--\ 'backend' : 'nvim',
+--\}
 
 set_var({
-    variable = "tex_flavor",
-    value = "latex"
+	variable = "tex_flavor",
+	value = "latex",
 })
